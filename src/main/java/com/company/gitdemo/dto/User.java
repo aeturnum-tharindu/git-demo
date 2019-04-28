332fb62621d85088fa7bd8c135fd83c004c1ac45
aeturnum-tharindu <tharindu.d@aeturnum.com>
2019-04-28 10:31:46 +0530

    Revert "Revert "name property set and get methods.""
    This reverts commit 63cf3ef1ce19c67c9205c2119fce72b5a7cf8e8b.

--- src/main/java/com/company/gitdemo/dto/User.java
+++ src/main/java/com/company/gitdemo/dto/User.java
@@ -6,6 +6,14 @@ import org.springframework.stereotype.Component;
 public class User {
 
 	private String name;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	private String uid;
 	
     private User() {
