9bab38bb6a62be1db1fd817773fb6ed602eb5dea
aeturnum-tharindu <tharindu.d@aeturnum.com>
2020-08-02 13:38:06 +0530

    test commit

--- src/main/java/com/company/gitdemo/controller/UserCotroller.java
+++ src/main/java/com/company/gitdemo/controller/UserCotroller.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserCotroller {
 
+    /**
+     getUsers details
+    */
     @PostMapping("/user/getUsers")
     public void getUsers() {
 
